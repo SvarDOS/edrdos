4e00c6cd9f91fad22a497544559f8d2630f7ac84
Bernd Boeckmann <bernd-github@boeckmann.io>
2024-07-20 14:29:17 +0200

    add country.sys and licenses to image creation script

--- image/mkimage.sh
+++ image/mkimage.sh
@@ -8,7 +8,11 @@ LABEL=EDR-DOS
 dd if=/dev/zero of=$IMAGE bs=512 count=2880
 mformat -i $IMAGE -v $LABEL
 dd if=bootsect.144 of=$IMAGE bs=512 count=1 conv=notrunc
+mmd -i $IMAGE ::/license
 mcopy -i $IMAGE ../dist/drbio.sys ::/
 mcopy -i $IMAGE ../dist/drdos.sys ::/
 mcopy -i $IMAGE ../dist/command.com ::/
+mcopy -i $IMAGE ../dist/country.sys ::/
 mcopy -i $IMAGE ../dist/sys.com ::/
+mcopy -i $IMAGE ../dist/gpl.txt ::/license/gpl.txt
+mcopy -i $IMAGE ../license.htm ::/license/edrdos.htm
