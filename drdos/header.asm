53c026404f71e23dce107859dd116706683b6e1e
Bernd Boeckmann <bernd-github@boeckmann.io>
2024-08-24 12:45:18 +0200

    make clear zero-padding in header.asm is no JWasm artifact

--- drdos/header.asm
+++ drdos/header.asm
@@ -137,7 +137,7 @@ patch_version	dw	DOSINROM+0000h		; 0022h sub-version (was SYSDAT length)
 						;       control table.
 		dw	NoYesChars		; 0028h offset in data of pointers to 
 						; default country info.
-		db 6 dup (0)	; REMOVE AFTER JWASM CONVERSION
+		db 6 dup (0)			; (paragraph alignment)
 PCM_HEADER ends
 
 ;****************************************************************************
