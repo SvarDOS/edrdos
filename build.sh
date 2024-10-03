735b1dee3f4585bb4025126fc9a51ebc6628e9bd
Bernd Boeckmann <bernd-git@boeckmann.io>
2024-10-03 14:44:14 +0200

    build.sh: make sure git exists, adapt git workflow to new default build

--- build.sh
+++ build.sh
@@ -3,7 +3,15 @@
 # Helper build-script to inject current Git revision into the build process
 # and build the kernel
 
-if [ "$1" = "dualfile" ]; then
+
+if ! command -v git >/dev/null 2>&1; then
+	echo "error: git not found";
+	exit 1
+fi
+
+if [ "$1" = "singlefile" ]; then
+	FLAGS="SINGLEFILE=1"
+elif [ "$1" = "dualfile" ]; then
 	FLAGS="SINGLEFILE=0"
 else
 	FLAGS="SINGLEFILE=1"
