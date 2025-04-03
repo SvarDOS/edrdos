SvarDOS/edrdos

commit 6f90911256b5a24b453a151aa998c8b6f4b44202

start work on making FAT+ a build time option
An assembler definition FATPLUS is introduced which conditionally
enables FAT+. Currently there is no provision to define FATPLUS via
makefile, so FAT+ is currently unconditionally disabled when building.

drdos/redir.asm
@@ -154,10 +154,12 @@ redir_dhndl_accept:
 	mov	word ptr current_filepos,ax
 	mov	ax,es:word ptr DHNDL_POS+2[bx]
 	mov	word ptr current_filepos+2,ax
+ifdef FATPLUS
 	mov	ax,es:word ptr DHNDL_POSX[bx]
 	mov	word ptr current_filepos+4,ax
 	mov	ax,es:word ptr DHNDL_POSX+2[bx]
 	mov	word ptr current_filepos+6,ax
+endif
 	pop	ds
 	jmp	redir_accept		; now we can process the call
 
@@ -965,6 +967,7 @@ redir_lseek10:				; seek mode 1: relative to position
 ;	adc	cx,es:DHNDL_POSHI[di]
 	add	es:DHNDL_POSLO[di],dx
 	adc	es:DHNDL_POSHI[di],cx
+ifdef FATPLUS
 	adc	es:DHNDL_POSXLO[di],0
 	adc	es:DHNDL_POSXHI[di],0
 	test	cx,8000h		; negative offset?
@@ -989,13 +992,16 @@ redir_lseek16:
 redir_lseek17:
 	mov	dx,es:DHNDL_POSLO[di]
 	mov	cx,es:DHNDL_POSHI[di]
+endif
 	jmp	redir_lseek90
 
 redir_lseek20:				; seek mode 0: set absolute position
 	mov	es:DHNDL_POSLO[di],dx	; set new file offset
 	mov	es:DHNDL_POSHI[di],cx	; SI = error code/0 at this point
+ifdef FATPLUS
 	mov	es:DHNDL_POSXLO[di],ax
 	mov	es:DHNDL_POSXHI[di],ax
+endif
 ;	jmp	redir_lseek90
 
 redir_lseek90:

drbio/doshndl.def
@@ -81,7 +81,7 @@ DHNDL_DEVPTR	equ	dword ptr 07h		; 07 - pointer to device
 DHNDL_DEVOFF	equ	word ptr 07h		; 07 - offset of device
 DHNDL_DEVSEG	equ	word ptr 09h		; 09 - segment of device
 DHNDL_BLK1	equ	word ptr 0Bh		; 0B - first cluster in file
-						;      (lower 32 bits)
+						;      (lower 16 bits)
 DHNDL_TIME	equ	word ptr 0Dh		; 0D - file time stamp
 DHNDL_DATE	equ	word ptr 0Fh		; 0F - file date stamp
 DHNDL_SIZE	equ	dword ptr 11h		; 11 - file length
@@ -93,9 +93,9 @@ DHNDL_POS	equ	dword ptr 15h		; 15 - current file position
 DHNDL_POSLO	equ	word ptr 15h
 DHNDL_POSHI	equ	word ptr 17h
 DHNDL_IDX	equ	word ptr 19h		; 19 - relative cluster within file of last read
-						;      (lower 32 bits)
+						;      (lower 16 bits)
 DHNDL_DBLK	equ	word ptr 1Bh		; 1B - cluster # of dir entry
-						;      (lower 32 bits)
+						;      (lower 16 bits)
 DHNDL_DCNTHI	equ	byte ptr 1Eh		; 1E - dir offset # within cluster
 DHNDL_DCNTLO	equ	byte ptr 1Fh		; 1F - dir offset # within cluster
 DHNDL_NAME	equ	byte ptr 20h		; 20 - File/Device Name
@@ -105,16 +105,17 @@ DHNDL_UID	equ	word ptr 2Fh		; 2F - Owning Machine ID
 DHNDL_PSP	equ	word ptr 31h		; 31 - Owning PSP
 DHNDL_SHARE	equ	word ptr 33h		; 33 - Offset of sharing record
 DHNDL_BLK	equ	word ptr 35h		; 35 - absolute cluster of last read
-						;      (lower 32 bits)
+						;      (lower 16 bits)
 						; 37 - dword reserved for IFS
 DHNDL_BLK1H	equ	word ptr 3Bh		; 3B - first cluster in file
-						;      (higher 32 bits)
+						;      (higher 16 bits)
 DHNDL_IDXH	equ	word ptr 3Dh		; 3D - relative cluster within file of last read
-						;      (higher 32 bits)
+						;      (higher 16 bits)
 DHNDL_DBLKH	equ	word ptr 3Fh		; 3F - cluster # of dir entry
-						;      (higher 32 bits)
+						;      (higher 16 bits)
 DHNDL_BLKH	equ	word ptr 41h		; 41 - absolute cluster of last read
-						;      (higher 32 bits)
+						;      (higher 16 bits)
+ifdef FATPLUS
 DHNDL_SIZEX	equ	dword ptr 43h		; 43 - file length
 						;      (higher 32 bits)
 DHNDL_SIZEXLO	equ	word ptr 43h
@@ -124,6 +125,10 @@ DHNDL_POSX	equ	dword ptr 47h		; 47 - current file position
 DHNDL_POSXLO	equ	word ptr 47h
 DHNDL_POSXHI	equ	word ptr 49h
 DHNDL_LEN	equ	4Bh
+else
+DHNDL_LEN	equ	43h
+endif
+
 
 ; With DOS 3 structures _DBLK is a 16 bit
 

drdos/bdevio.asm
@@ -210,8 +210,10 @@ byteoff		dw	0		; fdosrw local variable
 		dw	0		; byte offset with file
 		dw	0,0
 
-fsize		dw	0,0,0,0		; used for file size calculations
-lastpos		dw	0,0,0,0		; last position that has been written
+fsize		dw	0,0		; used for file size calculations
+		dw	0,0
+lastpos		dw	0,0		; last position that has been written
+		dw	0,0
 
 blk		dw	0,0		; current cluster of filepos
 blkidx		dw	0,0		; current cluster index within file
@@ -375,10 +377,12 @@ fdrw_noerror:
 	sub	ax,lastpos
 	mov	ax,byteoff+2
 	sbb	ax,lastpos+2
+ifdef FATPLUS
 	mov	ax,byteoff+4
 	sbb	ax,lastpos+4
 	mov	ax,byteoff+6
 	sbb	ax,lastpos+6
+endif
 	 jc	fdrw_buffered
 	call	deblock_rw_npr
 	jmp	fdrw_more
@@ -394,8 +398,10 @@ fdrw_more:
 
 	add	byteoff,ax		; adjust file offset
 	adc	byteoff+2,0
+ifdef FATPLUS
 	adc	byteoff+4,0
 	adc	byteoff+6,0
+endif
 
 	push	ax
 	mov	ax,psecsiz
@@ -404,19 +410,23 @@ fdrw_more:
 	sub	ax,byteoff
 	mov	ax,lastpos+2
 	sbb	ax,byteoff+2
+ifdef FATPLUS
 	mov	ax,lastpos+4
 	sbb	ax,byteoff+4
 	mov	ax,lastpos+6
 	sbb	ax,byteoff+6
+endif
 	 jnc	fdrw_nohigher
 	mov	ax,byteoff
 	mov	lastpos,ax
 	mov	ax,byteoff+2
 	mov	lastpos+2,ax
+ifdef FATPLUS
 	mov	ax,byteoff+4
 	mov	lastpos+4,ax
 	mov	ax,byteoff+6
 	mov	lastpos+6,ax
+endif
 
 fdrw_nohigher:
 	pop	ax
@@ -429,8 +439,10 @@ fdrw_exit:
 	mov	ax,fdos_ret		; get total xfered and update position
 	add	es:DHNDL_POSLO[bx],ax
 	adc	es:DHNDL_POSHI[bx],0
+ifdef FATPLUS
 	adc	es:DHNDL_POSXLO[bx],0
 	adc	es:DHNDL_POSXHI[bx],0
+endif
 	test	fdrwflg,1
 	 jnz	fdrw_return		; skip if reading
 ;	mov	ax,byteoff		; has the file grown ?
@@ -443,12 +455,14 @@ fdrw_exit:
 	mov	ax,byteoff+2
 	sbb	ax,es:DHNDL_SIZEHI[bx]
 	mov	fsize+2,ax
+ifdef FATPLUS
 	mov	ax,byteoff+4
 	sbb	ax,es:DHNDL_SIZEXLO[bx]
 	mov	fsize+4,ax
 	mov	ax,byteoff+6
 	sbb	ax,es:DHNDL_SIZEXHI[bx]
 	mov	fsize+6,ax
+endif
 	 jb	fdrw_nobigger		; yes, update the file size
 ;	add	es:DHNDL_SIZELO[bx],ax
 ;	adc	es:DHNDL_SIZEHI[bx],dx
@@ -456,10 +470,12 @@ fdrw_exit:
 	add	es:DHNDL_SIZELO[bx],ax
 	mov	ax,fsize+2
 	adc	es:DHNDL_SIZEHI[bx],ax
+ifdef FATPLUS
 	mov	ax,fsize+4
 	adc	es:DHNDL_SIZEXLO[bx],ax
 	mov	ax,fsize+6
 	adc	es:DHNDL_SIZEXHI[bx],ax
+endif
 fdrw_nobigger:
 	call	timestamp_dhndl		; record the current time
 	test	es:DHNDL_MODE[bx],DHM_COMMIT
@@ -550,10 +566,12 @@ fdw_t60:
 	mov	es:DHNDL_SIZELO[bx],ax
 	mov	ax,byteoff+2
 	mov	es:DHNDL_SIZEHI[bx],ax
+ifdef FATPLUS
 	mov	ax,byteoff+4
 	mov	es:DHNDL_SIZEXLO[bx],ax
 	mov	ax,byteoff+6
 	mov	es:DHNDL_SIZEXHI[bx],ax
+endif
 	xor	ax,ax			; cause reads/writes to scan
 	mov	es:DHNDL_BLK[bx],ax	;   block chain from start
 	mov	es:DHNDL_BLKH[bx],ax
@@ -609,6 +627,7 @@ fdrw_p20:
 	mov	byteoff,ax
 	mov	ax,es:DHNDL_POSHI[bx]
 	mov	byteoff+2,ax
+ifdef FATPLUS
 	mov	ax,es:DHNDL_POSXLO[bx]
 	cmp	ax,63			; greater than 256 GB-1
 	 jbe	fdrw_p25
@@ -622,14 +641,17 @@ fdrw_p25:
 	jmp	fdrw_p40
 fdrw_p27:
 	mov	byteoff+6,ax
+endif
 	mov	ax,es:DHNDL_SIZELO[bx]	; copy file size
 	mov	lastpos,ax
 	mov	ax,es:DHNDL_SIZEHI[bx]
 	mov	lastpos+2,ax
+ifdef FATPLUS
 	mov	ax,es:DHNDL_SIZEXLO[bx]
 	mov	lastpos+4,ax
 	mov	ax,es:DHNDL_SIZEXHI[bx]
 	mov	lastpos+6,ax
+endif
 ;	mov	cx,clsize
 ;	mov	ax,lastcl
 ;	mul	cx			; DX:AX = maximum size of disk
@@ -644,8 +666,20 @@ fdrw_p27:
 ;	pop	cx
 	pop	fsize
 	pop	fsize+2
+ifdef FATPLUS
 	pop	fsize+4
 	pop	fsize+6
+else
+	pop	ax
+	pop	cx
+	or	ax,cx
+	 jz	fdrw_p28		; disk space less than 2^32 bytes?
+	xor	ax,ax
+	dec	ax
+	mov	fsize,ax		; else ensure file size stays below
+	mov	fsize+2,ax		; 2^32 bytes
+fdrw_p28:
+endif
 ;	add	sp,10			; clean up the stack again
 	add	sp,8			; clean up the stack again
 ;	sub	ax,byteoff
@@ -655,10 +689,12 @@ fdrw_p27:
 	sub	ax,byteoff
 	mov	ax,fsize+2
 	sbb	ax,byteoff+2
+ifdef FATPLUS
 	mov	ax,fsize+4
 	sbb	ax,byteoff+4
 	mov	ax,fsize+6
 	sbb	ax,byteoff+6
+endif
 	 jc	fdrw_p30
 ;	mov	ax,byteoff		; DX:AX = current file size
 ;	mov	dx,byteoff+WORD
@@ -713,20 +749,24 @@ fdrw_size:
 	mov	ax,es:DHNDL_SIZEHI[bx]
 	sbb	ax,byteoff+2
 	mov	fsize+2,ax
+ifdef FATPLUS
 	mov	ax,es:DHNDL_SIZEXLO[bx]
 	sbb	ax,byteoff+4
 	mov	fsize+4,ax
 	mov	ax,es:DHNDL_SIZEXHI[bx]
 	sbb	ax,byteoff+6
 	mov	fsize+6,ax
+endif
 	 jb	fdrw_s40
 ;	sub	ax,fdrwcnt		; will we be going beyond EOF ?
 ;	sbb	dx,0
 	mov	ax,fdrwcnt
 	sub	fsize,ax
 	sbb	fsize+2,0
+ifdef FATPLUS
 	sbb	fsize+4,0
 	sbb	fsize+6,0
+endif
 	 jnb	fdrw_s10		; no, whole xfer is OK
 	test	fdrwflg,1		; check if we're reading
 	 jz	fdrw_s50		;  if we are just adjust the
@@ -787,12 +827,14 @@ fdwrite_extend:
 	mov	ax,byteoff+2
 	adc	ax,0
 	mov	fsize+2,ax
+ifdef FATPLUS
 	mov	ax,byteoff+4
 	adc	ax,0
 	mov	fsize+4,ax
 	mov	ax,byteoff+6
 	adc	ax,0
 	mov	fsize+6,ax
+endif
 ;	div	clsize			; AX whole blocks required
 ;	push	dx
 ;	push	ax

drdos/cdevio.asm
@@ -123,9 +123,11 @@ open_dev:
 	lea	di,DHNDL_DATRB[bx]	; point at start of area to fill
 	mov	cx,DHNDL_UID-DHNDL_DATRB
 	rep	stosb
+ifdef FATPLUS
 	lea	di,DHNDL_SIZEX[bx]
 	mov	cx,(DHNDL_LEN - DHNDL_SIZEX) / 2
 	rep	stosw			; clear 64-bit high dwords size/seek
+endif
 	mov	es:DHNDL_SHARE[bx],ax	; also zap share word	
 	pop	es:DHNDL_DEVOFF[bx]
 	pop	es:DHNDL_DEVSEG[bx]	; save device driver address

drdos/doshndl.def
@@ -81,7 +81,7 @@ DHNDL_DEVPTR	equ	dword ptr 07h		; 07 - pointer to device
 DHNDL_DEVOFF	equ	word ptr 07h		; 07 - offset of device
 DHNDL_DEVSEG	equ	word ptr 09h		; 09 - segment of device
 DHNDL_BLK1	equ	word ptr 0Bh		; 0B - first cluster in file
-						;      (lower 32 bits)
+						;      (lower 16 bits)
 DHNDL_TIME	equ	word ptr 0Dh		; 0D - file time stamp
 DHNDL_DATE	equ	word ptr 0Fh		; 0F - file date stamp
 DHNDL_SIZE	equ	dword ptr 11h		; 11 - file length
@@ -93,9 +93,9 @@ DHNDL_POS	equ	dword ptr 15h		; 15 - current file position
 DHNDL_POSLO	equ	word ptr 15h
 DHNDL_POSHI	equ	word ptr 17h
 DHNDL_IDX	equ	word ptr 19h		; 19 - relative cluster within file of last read
-						;      (lower 32 bits)
+						;      (lower 16 bits)
 DHNDL_DBLK	equ	word ptr 1Bh		; 1B - cluster # of dir entry
-						;      (lower 32 bits)
+						;      (lower 16 bits)
 DHNDL_DCNTHI	equ	byte ptr 1Eh		; 1E - dir offset # within cluster
 DHNDL_DCNTLO	equ	byte ptr 1Fh		; 1F - dir offset # within cluster
 DHNDL_NAME	equ	byte ptr 20h		; 20 - File/Device Name
@@ -105,16 +105,17 @@ DHNDL_UID	equ	word ptr 2Fh		; 2F - Owning Machine ID
 DHNDL_PSP	equ	word ptr 31h		; 31 - Owning PSP
 DHNDL_SHARE	equ	word ptr 33h		; 33 - Offset of sharing record
 DHNDL_BLK	equ	word ptr 35h		; 35 - absolute cluster of last read
-						;      (lower 32 bits)
+						;      (lower 16 bits)
 						; 37 - dword reserved for IFS
 DHNDL_BLK1H	equ	word ptr 3Bh		; 3B - first cluster in file
-						;      (higher 32 bits)
+						;      (higher 16 bits)
 DHNDL_IDXH	equ	word ptr 3Dh		; 3D - relative cluster within file of last read
-						;      (higher 32 bits)
+						;      (higher 16 bits)
 DHNDL_DBLKH	equ	word ptr 3Fh		; 3F - cluster # of dir entry
-						;      (higher 32 bits)
+						;      (higher 16 bits)
 DHNDL_BLKH	equ	word ptr 41h		; 41 - absolute cluster of last read
-						;      (higher 32 bits)
+						;      (higher 16 bits)
+ifdef FATPLUS
 DHNDL_SIZEX	equ	dword ptr 43h		; 43 - file length
 						;      (higher 32 bits)
 DHNDL_SIZEXLO	equ	word ptr 43h
@@ -124,6 +125,10 @@ DHNDL_POSX	equ	dword ptr 47h		; 47 - current file position
 DHNDL_POSXLO	equ	word ptr 47h
 DHNDL_POSXHI	equ	word ptr 49h
 DHNDL_LEN	equ	4Bh
+else
+DHNDL_LEN	equ	43h
+endif
+
 
 ; With DOS 3 structures _DBLK is a 16 bit
 

drdos/fdos.asm
@@ -377,8 +377,10 @@ BDOS_CODE	segment public byte 'CODE'
 	extrn	update_fat:NEAR
 	extrn	write_dev:near		; write to character device
 	extrn	zeroblk:near		; fill cluster with 0's
+ifdef FATPLUS
 	extrn	is_lfn:near		; check if long filename entry
 	extrn	del_lfn:near		; delete long filenames
+endif
 
 	public	bpb2ddsc		; build DDSC from a BPB
 	Public	check_slash

drdos/fioctl.asm
@@ -359,8 +359,10 @@ ioctl6:		; file input status
 ioctl6_10:				; disk files/network devices
 	push	es:DHNDL_POSLO[bx]	; save current position in file
 	push	es:DHNDL_POSHI[bx]	;  so we can read ahead
+ifdef FATPLUS
 	push	es:DHNDL_POSXLO[bx]
 	push	es:DHNDL_POSXHI[bx]
+endif
 	push	es
 	push	bx			; save DHNDL_ too..
 	push	bp			; save stack frame
@@ -392,8 +394,10 @@ ioctl6_30:
 	pop	bp			; recover stack frame
 	pop	bx			; rewind DHNDL_POS to where
 	pop	es
+ifdef FATPLUS
 	pop	es:DHNDL_POSXHI[bx]	;  it was before we started
 	pop	es:DHNDL_POSXLO[bx]
+endif
 	pop	es:DHNDL_POSHI[bx]
 	pop	es:DHNDL_POSLO[bx]
 	mov	ax,1a00h		; assume not ready

drdos/funcs.fdo
@@ -792,10 +792,12 @@ fdos_read:
 	mov	dx,ax
 	mov	ax,es:DHNDL_POSHI[bx]
 	sbb	ax,es:DHNDL_SIZEHI[bx]
+ifdef FATPLUS
 	mov	ax,es:DHNDL_POSXLO[bx]
 	sbb	ax,es:DHNDL_SIZEXLO[bx]
 	mov	ax,es:DHNDL_POSXHI[bx]
 	sbb	ax,es:DHNDL_SIZEXHI[bx]
+endif
 ;	 jb	read2			; beyond EOF already, no action required
 ;	 ja	read2			; more the 64K to go, no problems
 	 jnz	read2
@@ -973,65 +975,63 @@ endif
 ;	AX:	0000 or error code ( < 0)
 ;	offset:	new offset
 
-fdos_lseek:
+fdos_lseek proc
 ;----------
 	call	vfy_dhndl_ptr		; check file handle #
 	call	redir_dhndl_offer
 	call	local_disk		; get MXdisk, switch stack
 	test	es:DHNDL_ATTR[bx],DHAT_DEV
-	 jnz	lseek_dev		; skip a part if character device
+	 jnz	@@dev			; skip a part if character device
 	call	check_handle		; check if legal file handle
-lseek_dev:
+@@dev:
 	mov	ax,fdos_pb+4		; get 32-bit file offset
 	mov	dx,fdos_pb+6		; into AX,DX
 	mov	cx,fdos_pb+8		; get seek mode
-	 jcxz	lseek4			; seek from beginning
+	 jcxz	@@seek_beg		; seek from beginning
 	dec	cx
-	 jz	lseek2			; seek from current position
+	 jz	@@seek_cur		; seek from current position
 	dec	cx
-	 jz	lseek3			; seek from end
-	mov	ax,ED_DATA		; else invalid seek mode
+	 jz	@@seek_end		; seek from end
+@@err:	mov	ax,ED_DATA		; else invalid seek mode
 	jmp	fdos_error		; return error code
-lseek1:
 	ret				; return error code
 
-lseek2:					; seek mode 1: relative to position
-;	add	ax,es:DHNDL_POSLO[bx]
-;	adc	dx,es:DHNDL_POSHI[bx]
-	add	es:DHNDL_POSLO[bx],ax
-	adc	es:DHNDL_POSHI[bx],dx
-	adc	es:DHNDL_POSXLO[bx],0
-	adc	es:DHNDL_POSXHI[bx],0
-	test	dx,8000h		; negative offset?
-	 jz	lseek2a
-	add	es:DHNDL_POSXLO[bx],0ffffh; yes, then extend the sign to 64-bit
-	adc	es:DHNDL_POSXHI[bx],0ffffh
-lseek2a:
-	jmp	lseek4a			; update new position
+@@seek_beg:				; seek mode 0: set absolute position
+	mov	es:DHNDL_POSLO[bx],ax
+	mov	es:DHNDL_POSHI[bx],dx
+	mov	fdos_pb+4,ax		; set return values
+	mov	fdos_pb+6,dx
+ifdef FATPLUS
+	mov	es:DHNDL_POSXLO[bx],cx
+	mov	es:DHNDL_POSXHI[bx],cx
+endif
+	ret
 
-lseek3:					; seek mode 2: relative to end
-;	add	ax,es:DHNDL_SIZELO[bx]
-;	adc	dx,es:DHNDL_SIZEHI[bx]	; add file size + offset
+@@seek_end:				; seek mode 2: relative to end
 	push	ax
 	mov	ax,es:DHNDL_SIZELO[bx]
 	mov	es:DHNDL_POSLO[bx],ax
 	mov	ax,es:DHNDL_SIZEHI[bx]
 	mov	es:DHNDL_POSHI[bx],ax
+ifdef FATPLUS
 	mov	ax,es:DHNDL_SIZEXLO[bx]
 	mov	es:DHNDL_POSXLO[bx],ax
 	mov	ax,es:DHNDL_SIZEXHI[bx]
 	mov	es:DHNDL_POSXHI[bx],ax
+endif
 	pop	ax
-	jmp	lseek2
-lseek4:					; seek mode 0: set absolute position
-	mov	es:DHNDL_POSLO[bx],ax
-	mov	es:DHNDL_POSHI[bx],dx
-	mov	fdos_pb+4,ax		; set return values
-	mov	fdos_pb+6,dx
-	mov	es:DHNDL_POSXLO[bx],cx
-	mov	es:DHNDL_POSXHI[bx],cx
-	ret
-lseek4a:
+
+@@seek_cur:				; seek mode 1: relative to position
+	add	es:DHNDL_POSLO[bx],ax
+	adc	es:DHNDL_POSHI[bx],dx
+ifdef FATPLUS
+	adc	es:DHNDL_POSXLO[bx],0
+	adc	es:DHNDL_POSXHI[bx],0
+	test	dx,8000h		; negative offset?
+	 jz	@f
+	add	es:DHNDL_POSXLO[bx],0ffffh; yes, then extend the sign to 64-bit
+	adc	es:DHNDL_POSXHI[bx],0ffffh
+@@:
 	cmp	es:DHNDL_POSXLO[bx],0
 	 jne	lseek4b
 	cmp	es:DHNDL_POSXHI[bx],0
@@ -1045,14 +1045,17 @@ lseek4c:
 	xor	ax,ax
 	dec	ax
 	mov	dx,ax
-	jmp	lseek4e
+	jmp	@@update
 lseek4d:
+endif
 	mov	ax,es:DHNDL_POSLO[bx]
 	mov	dx,es:DHNDL_POSHI[bx]
-lseek4e:
+@@update:
 	mov	fdos_pb+4,ax		; set return values
 	mov	fdos_pb+6,dx
 	ret
+fdos_lseek endp
+
 
 ;	GET/SET FILE ATTRIBUTES (CHMOD)
 
@@ -1851,6 +1854,7 @@ file_update:
 	xchg	ax,di			; DI -> directory entry in buffer
 	or	DATTS[di],DA_ARCHIVE	; mark file as modified
 ;;;	call	timestamp_dhndl		; record the current time
+ifdef FATPLUS
 	mov	ax,es:DHNDL_SIZEXLO[bx]	; get extended 6 bits for FAT+
 	mov	ah,al			; shift the bits in place
 	and	al,00000111b
@@ -1862,6 +1866,7 @@ file_update:
 	and	al,00011000b		; save bits used WinNT
 	or	al,ah			; combine them with FAT+ bits
 	mov	DSIZEX[di],al		; and store them again
+endif
 	cmp	dosfat,FAT32		; is this a FAT32 file system?
 	 jne	file_update05		; no, then skip this entry
 	mov	ax,es:DHNDL_BLK1H[bx]	; get high word of 1st block in file
@@ -2531,7 +2536,9 @@ move60:					; moving to different directory
 	 jnz	move_access		; so return error if new path
 	call	allocdir		; allocate a directory entry
 move70:
+ifdef FATPLUS
 	call	del_lfn			; make sure no LFN is before it
+endif
 	mov	si,offset info_fcb+1	; get pointer to new name
 	mov	di,dirp			; get pointer to directory entry
 	mov	cx,11			; copy new name into buffer
@@ -2550,7 +2557,9 @@ move70:
 	mov	cx,HDS_LEN		; so we can get old entry again
 	rep	movsb			;    (time to get rid of it)
 	call	move_seek		; seek the original entry
+ifdef FATPLUS
 	call	del_lfn			; and delete LFN if one exists
+endif
 	mov	bx,dirp
 	mov	DNAME[bx],0E5h		; and bye, bye! it goes...
 	call	update_dir		; update the old directory entry

commit 9b75a6bd658006bd52a7824633924dff1134085a

fix redir_seek broken by commit 6f909112 (#145)
A wrong places conditional resulted in absolute seek instead of relative

drdos/redir.asm
@@ -990,9 +990,9 @@ redir_lseek16:
 	mov	cx,dx
 	jmp	redir_lseek90
 redir_lseek17:
+endif
 	mov	dx,es:DHNDL_POSLO[di]
 	mov	cx,es:DHNDL_POSHI[di]
-endif
 	jmp	redir_lseek90
 
 redir_lseek20:				; seek mode 0: set absolute position