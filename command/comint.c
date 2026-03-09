SvarDOS/edrdos

commit d6a0051b10b2a12e7873d364e5be60bedefbc180

command.com: remove non-triggered bug in SET
off-by-one error: equal sign (=) was part of key

command/comint.c
@@ -1638,8 +1638,6 @@ GLOBAL VOID CDECL cmd_set( BYTE *s )
       return;
    } /* a syntax error.		*/
 
-   s++;
-
 #if 0
 	/* msdos doesn't do this */
 
@@ -1658,7 +1656,9 @@ GLOBAL VOID CDECL cmd_set( BYTE *s )
       crlfflg = YES;
       return;
    }
-   if ( ( *s-- = c ) != 0 ) { /* Add the definition to the end*/
+   *s++ = c;
+
+   if ( *s != 0 ) { /* Add the definition to the end*/
       /* of the environment if the new*/
       if ( env_ins( key ) ) {   /* definition is not NULL	*/
          printf( MSG_ENVFULL ); /* check for an error.		*/

commit 579ab148df4d6af82752155137e0cad6a31b1a34

prevent SET command from creating duplicate variables
regression introduced by d6a0051b10b2a12e7873d364e5be60bedefbc180

We have to include = as part of the variable name given to
env_del, because get_key does not check for = otherwise, and thus may
return a variable for which the one to be deleted is a prefix.

command/comint.c
@@ -1631,6 +1631,8 @@ GLOBAL VOID CDECL cmd_set( BYTE *s )
       return;
    } /* a syntax error.		*/
 
+   s++;                        /* skip over = (include in key name) */
+
 #if 0
 	/* msdos doesn't do this */
 
@@ -1649,7 +1651,7 @@ GLOBAL VOID CDECL cmd_set( BYTE *s )
       crlfflg = YES;
       return;
    }
-   *s++ = c;
+   *s = c;    /* recover first character of value */
 
    if ( *s != 0 ) { /* Add the definition to the end*/
       /* of the environment if the new*/